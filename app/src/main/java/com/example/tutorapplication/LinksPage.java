088c2b7478d00b18576f724961efdd136a468e67
Alex Hobby <ahobby@rocketmail.com>
2020-09-12 14:41:19 -0400

    added links button, small fixes

---
+++ app/src/main/java/com/example/tutorapplication/LinksPage.java
@@ -0,0 +1,14 @@
+package com.example.tutorapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinksPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_links_page);
+    }
+}
